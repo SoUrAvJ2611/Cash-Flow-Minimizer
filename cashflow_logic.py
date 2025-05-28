e5ac559ddf6890fc1520330a469960b9cdc6ec5e
SoUrAvJ2611 <souravv2611@gmail.com>
2025-05-28 09:39:59 +0530

    Update cashflow_logic.py

--- cashflow_logic.py
+++ cashflow_logic.py
@@ -1,42 +1,49 @@
 # cashflow_logic.py
 
+import heapq
+
 class CashFlowMinimizer:
-    def __init__(self, names, matrix):  # fixed constructor
+
+    def __init__(self, names, matrix):  
         self.names = names
         self.matrix = matrix
         self.n = len(names)
 
     def minimize(self):
         net_balance = [0] * self.n
+
         for i in range(self.n):
             for j in range(self.n):
-                net_balance[i] += self.matrix[j][i] - self.matrix[i][j]
+                net_balance[i] += self.matrix[i][j] - self.matrix[j][i]
 
         creditors = []
         debtors = []
+
         for i in range(self.n):
             if net_balance[i] > 0:
-                creditors.append((i, net_balance[i])) 
+                heapq.heappush(creditors, (-net_balance[i], i))
             elif net_balance[i] < 0:
-                debtors.append((i, -net_balance[i]))
+                heapq.heappush(debtors, (net_balance[i], i))
 
         result = []
-        i, j = 0, 0
-        while i < len(creditors) and j < len(debtors):
-            creditor_index, credit = creditors[i]
-            debtor_index, debt = debtors[j]
-            settled_amount = min(credit, debt)
+
+        while creditors and debtors:
+            credit, creditor_index = heapq.heappop(creditors)
+            debt, debtor_index = heapq.heappop(debtors)
+
+            settled_amount = min(-credit, -debt)
 
             result.append(
-                f"{self.names[creditor_index]} will pay ₹{round(settled_amount, 2)} to {self.names[debtor_index]}"
+                f"{self.names[debtor_index]} will pay ₹{round(settled_amount, 2)} to {self.names[creditor_index]}"
             )
 
-            creditors[i] = (creditor_index, credit - settled_amount)
-            debtors[j] = (debtor_index, debt - settled_amount)
+            remaining_credit = -credit - settled_amount
+            remaining_debt = -debt - settled_amount
 
-            if creditors[i][1] == 0:
-                i += 1
-            if debtors[j][1] == 0:
-                j += 1
+            if remaining_credit > 0:
+                heapq.heappush(creditors, (-remaining_credit, creditor_index))
+            if remaining_debt > 0:
+                heapq.heappush(debtors, (-remaining_debt, debtor_index))
 
         return result
+
